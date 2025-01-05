f980f32e4287dc021e8a9e38e56cec5ff22a2e24
moursalinme <me.moursalin@gmail.com>
2025-01-05 18:19:01 +0600

    feat(service): implement getSpeciesByName in Species Service

--- backend/src/main/java/com/petstore/backend/service/impl/SpeciesServiceImpl.java
+++ backend/src/main/java/com/petstore/backend/service/impl/SpeciesServiceImpl.java
@@ -1,17 +1,36 @@
 package com.petstore.backend.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.petstore.backend.entity.Species;
+import com.petstore.backend.repository.SpeciesRepository;
 import com.petstore.backend.service.SpeciesService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SpeciesServiceImpl implements SpeciesService {
+
+    private final SpeciesRepository speciesRepository;
 
     @Override
     public Species getSpeciesByName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSpeciesByName'");
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("Name must not be null, empty, or whitespace");
+        }
+
+        Optional<Species> species = speciesRepository.findByName(name);
+
+        if (species.isPresent()) {
+            return species.get();
+        }
+        Species entity = Species.builder().name(name).build();
+        entity = speciesRepository.save(entity);
+        return entity;
     }
 
 }
