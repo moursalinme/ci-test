555dc57de5b8d4f0400d4a6a9a797e96806ea38c
moursalinme <me.moursalin@gmail.com>
2025-01-05 16:07:57 +0600

    feat(dto): add builder for petRequest

--- backend/src/main/java/com/petstore/backend/dto/request/PetRequest.java
+++ backend/src/main/java/com/petstore/backend/dto/request/PetRequest.java
@@ -7,12 +7,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PetRequest {
 
     @NotBlank(message = "Name is required")
