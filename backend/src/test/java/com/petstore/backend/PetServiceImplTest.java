37a341f188ee207cee94c04eb0488d8062382d4d
moursalinme <me.moursalin@gmail.com>
2025-01-05 16:49:58 +0600

    test(service): add tests for getAllPets

--- backend/src/test/java/com/petstore/backend/PetServiceImplTest.java
+++ backend/src/test/java/com/petstore/backend/PetServiceImplTest.java
@@ -3,10 +3,13 @@ package com.petstore.backend;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +25,7 @@ import com.petstore.backend.entity.Pet;
 import com.petstore.backend.entity.Species;
 import com.petstore.backend.enums.PetGender;
 import com.petstore.backend.enums.PetStatus;
+import com.petstore.backend.mapper.Mapper;
 import com.petstore.backend.repository.PetRepository;
 import com.petstore.backend.service.impl.PetServiceImpl;
 
@@ -95,4 +99,32 @@ public class PetServiceImplTest {
         assertThrows(IllegalArgumentException.class, () -> petService.getPetById(null));
     }
 
+    @Test
+    void getPetById_NegativePetId_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> petService.getPetById(-1L));
+    }
+
+    @Test
+    void getAllPets_NoPets_ShouldReturnEmptyList() {
+        when(petRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<PetResponse> pets = petService.getAllPets();
+
+        assertTrue(pets.isEmpty(), "Expected an empty list when no pets are available");
+        verify(petRepository).findAll();
+    }
+
+    @Test
+    void getAllPets_WithPets_ShouldReturnPetResponses() {
+        List<Pet> petEntities = List.of(testPet, testPet);
+        petEntities.get(1).setId(2L);
+        when(petRepository.findAll()).thenReturn(petEntities);
+
+        List<PetResponse> petResponses = petService.getAllPets();
+
+        assertEquals(petEntities.size(), petResponses.size(), "Expected petResponses size to match petEntities size");
+        assertEquals(Mapper.toPetResponse(testPet), petResponses.get(0));
+        assertEquals(Mapper.toPetResponse(testPet), petResponses.get(1));
+        verify(petRepository).findAll();
+    }
 }
