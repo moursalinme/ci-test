b07182d09b30ad37ffa7e0e4f6cf5d2a3a0349c5
moursalinme <me.moursalin@gmail.com>
2025-01-09 20:21:04 +0600

    feat(navbar): add navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
+import Navbar from "./component/Navbar";
+
 function App() {
   return (
     <>
-      <div className="text-xl">
-        Initial setup{" "}
-        <strong className="text-xl font-bold text-green-600">complete</strong>
-      </div>
+      <Navbar />
     </>
   );
 }
