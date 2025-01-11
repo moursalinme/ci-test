c37caadf527f29a1a347d8c018227a9464cb581e
moursalinme <me.moursalin@gmail.com>
2025-01-11 18:51:29 +0600

    feat(router): add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./component/Navbar";
-
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </React.Fragment>
   );
 }
 
