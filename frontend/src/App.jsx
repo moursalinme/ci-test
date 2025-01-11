f44efb12fc9050091649c8dd40254b535ba63bfb
moursalinme <me.moursalin@gmail.com>
2025-01-11 19:06:36 +0600

    feat(pages): add pages in routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./component/Navbar";
+import CreatePetPage from "./page/CreatePetPage";
+import EditPage from "./page/EditPage";
+import LandingPage from "./page/LandingPage";
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={LandingPage} />
+        <Route path="/add" element={CreatePetPage} />
+        <Route path="/edit" element={EditPage} />
       </Routes>
     </React.Fragment>
   );
