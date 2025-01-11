9038ea5abaa023178a304f5a892aceb714a2d7c3
moursalinme <me.moursalin@gmail.com>
2025-01-11 21:56:05 +0600

    feat(backend): add cors configs

---
+++ backend/src/main/java/com/petstore/backend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.petstore.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${FRONTEND_URL:http://localhost:5173}")
+    private String myFrontend;
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins(myFrontend)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
